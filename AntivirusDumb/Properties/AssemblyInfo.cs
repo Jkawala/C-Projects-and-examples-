b0d02bed653ab91c47300ea71f3aeb4b852d5a8c
James Kawala <spotkawala@gmail.com>
2024-01-22 23:37:24 +0000

    intitial Commit

---
+++ AntivirusDumb/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("AntivirusDumb")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AntivirusForm")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+
+[assembly: Guid("f0efc4d5-bf50-4aee-affe-4234cfc18b25")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
